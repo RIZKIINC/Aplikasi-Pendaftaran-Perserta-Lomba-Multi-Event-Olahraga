81002c8c4585037caa4f1bcbd56552e407877b32
RIZKIINC <rizkiilhami27@gmail.com>
2023-05-30 13:28:26 +0700

    memasukkan template

---
+++ resources/views/admin/layout/appadmin.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    @include('admin.layout.top')
+    @include('admin.layout.menu')
+    @include('admin.layout.dashboard')
+    @include('admin.layout.footer')
+
+</html>
