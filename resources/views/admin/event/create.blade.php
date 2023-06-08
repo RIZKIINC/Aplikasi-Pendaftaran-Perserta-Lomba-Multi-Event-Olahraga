c23059bb4e35afd222d3917d2e67ef04c3e8d7ed
Putrimeylsa <dedisetiadi306@gmail.com>
2023-06-08 15:10:14 +0700

    tambah option cabor

--- resources/views/admin/event/create.blade.php
+++ resources/views/admin/event/create.blade.php
@@ -10,17 +10,16 @@ Tambah Data Event Cabor
     {{ csrf_field() }}
     <div class="col-md-6 grid-margin stretch-card">
         <div class="card">
-        <div class="form-group row">
-    <label for="select1" class="col-4 col-form-label">Cabang Olahraga</label> 
-            <div class="col-8">
-                <select id="select1" name="cabang_olahraga_id" class="custom-select">
-                    @foreach($cabor as $c)
-                    <option value="{{$c}}">{{$c}}</option>
-                    @endforeach
-                </select>
-                </div>
-            </div>
             <div class="card-body">
+                <div class="form-group">
+                    <label>Cabang Olahraga</label>
+                    <select name="cabang_olahraga_id" class="form-control form-control-sm">
+                        <option value="">Pilih Event Cabor</option>
+                        @foreach ($cabor as $c)
+                        <option value="{{ $c }}">{{ $c }}</option>
+                        @endforeach
+                    </select>
+                </div>
                 <div class="form-group">
                     <label>Nomor Olahraga</label>
                     <input name="nomor_olahraga" type="text" class="form-control form-control-sm" placeholder="Masukkan Nomor Olahraga" aria-label="Nomor Olahraga">
@@ -29,10 +28,8 @@ Tambah Data Event Cabor
                 <div class="form-group">
                     <label>Nama Event</label>
                     <input name="nama_event" type="text" class="form-control form-control-sm" placeholder="Masukkan Nama Event" aria-label="Nama Event">
-
-                 
                 </div>
-               
+
                 <div class="form-group">
                     <label>Jenis Kelamin</label>
                     <input name="jenis_kelamin" type="text" class="form-control form-control-sm" placeholder="Jenis Kelamin" aria-label="Jenis Kelamin">
